49ae504c1686ed746c46d05b10bca8e35bc08c5e
James Ortiz <jamesaortiz75@gmail.com>
2018-12-14 02:10:59 -0500

    title class mini program

---
+++ title.java
@@ -0,0 +1,16 @@
+//==============================================
+// title.java
+// Author: James Ortiz
+// Prints the of a string object title in upper
+// case letters.
+//===============================================
+
+public class title
+{
+    public static void main(String[] args)
+    {
+	String title = "hello";
+	System.out.println(title.toUpperCase());
+    }//main
+}//class title
+
